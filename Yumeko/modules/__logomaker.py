3283dd4d54cdd1d33b862aa857a12617967234ec
Xayzo <deonseorang@gmail.com>
2022-03-01 00:14:21 +0700

    Update __logomaker.py

--- Yumeko/modules/__logomaker.py
+++ Yumeko/modules/__logomaker.py
@@ -248,11 +248,11 @@ LOGO_LINKS            = ["https://telegra.ph/file/d1838efdafce9fe611d0c.jpg",
                          "https://telegra.ph/file/9849b3940f063b065f4e3.jpg"
                          ]
 
-@register(pattern="^/logo ?(.*)")
+@register(pattern="^/notlogo ?(.*)")
 async def lego(event):
  quew = event.pattern_match.group(1)
  if event.sender_id != OWNER_ID and not quew:
-  await event.reply('`Please give me the text for the logo!`\n`Example /logo <pantek>`')
+  await event.reply('`Please give me the text for the logo!`\n`Example /logo <royzu>`')
   return
  pesan = await event.reply('`Creating your logo...`')
  try:
@@ -276,9 +276,9 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black")
     fname = "luna.png"
     img.save(fname, "png")
-    await telethn.send_file(event.chat_id, file=fname, caption = f"Made by [Luna ✨](https://t.me/lunatapibot)")         
+    await telethn.send_file(event.chat_id, file=fname, caption = f"Made by [Royzu 🔱](https://t.me/RoyzuRobot)")         
     await pesan.delete()
     if os.path.exists(fname):
             os.remove(fname)
  except Exception as e:
-    await event.reply(f'Error, Report @lunaXresso')
+    await event.reply(f'Error, Report @DionSupport')
