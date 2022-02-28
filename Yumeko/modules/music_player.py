f1e50ff967119689eb7cd06cc739981876f60d4d
Xayzo <deonseorang@gmail.com>
2022-03-01 00:54:46 +0700

    Update and rename music_player to music_player.py

---
+++ Yumeko/modules/music_player.py
@@ -0,0 +1,26 @@
+__mod_name__ = "Music Player"
+
+__help__ = """
+=>> *Member Commands* 🎧 
+❍ `/rplay` <song name/audio> - play song you requested from Youtube
+❍ `/vplay` <song name/video> - play video you requested from Youtube
+❍ `/playlist` - Show your playlist
+❍ `/song` <song name/link> - download songs you want from Youtube
+❍ `/vsong` or `/video` <video name/link> - download videos you want from Youtubd
+=>> *Admins only*
+❍ `/reload` - Refresh admin list
+❍ `/pause` - To pause Song play
+❍ `/vpause` - To pause Video streaming
+❍ `/resume` - To resume Song play
+❍ `/vresume` - To resume Video streaming
+❍ `/skip` - To play next Song
+❍ `/vskip` - To play next Video
+❍ `/end` - Will stop music play
+❍ `/vend` - Will stop Video streaming
+❍ `/userbotjoin` - invite assistant to your chat
+❍ `/userbotleave` - assistant will leave from your chat
+❍ `/msettings` - open music player settings panel
+=>>   *Use*
+1) Add @RoyzuAssistant in your group to admin with all permission
+2) Add Assistant I'd to your group
+"""
