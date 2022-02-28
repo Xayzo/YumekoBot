b6d2cfef859c09a2a0b71a8c938ab785e21685b1
Xayzo <deonseorang@gmail.com>
2022-03-01 00:33:02 +0700

    Update alive.py

--- Yumeko/modules/alive.py
+++ Yumeko/modules/alive.py
@@ -7,7 +7,7 @@ from pyrogram import __version__ as pyrover
 from Yumeko.events import register as MEMEK
 from Yumeko import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/e0040466048302df1a451.mp4"
+PHOTO = "https://telegra.ph/file/22d0cda0a988fff8e01ea.jpg"
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
